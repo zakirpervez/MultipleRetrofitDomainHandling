28ce9c5ab9018075a68cb32f7fbf2a47b375173b
Zakir.Mohammad <zmohammad@schoology.com>
2022-09-01 22:10:36 +0530

    hilt configured

---
+++ app/src/main/java/com/zandroid/multipleretrofitdomainhandling/MultiDomainApp.kt
@@ -0,0 +1,7 @@
+package com.zandroid.multipleretrofitdomainhandling
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MultiDomainApp: Application()
--- app/src/main/java/com/zandroid/multipleretrofitdomainhandling/ui/MainActivity.kt
+++ app/src/main/java/com/zandroid/multipleretrofitdomainhandling/ui/MainActivity.kt
@@ -1,8 +1,11 @@
-package com.zandroid.multipleretrofitdomainhandling
+package com.zandroid.multipleretrofitdomainhandling.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.zandroid.multipleretrofitdomainhandling.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
